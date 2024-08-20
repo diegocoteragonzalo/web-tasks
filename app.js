81f014214b34ce01aa9c11a7a5263c4e707ecc28
diegocoteragonzalo <diegocotera@gmail.com>
2024-08-20 19:44:44 +0200

    5.8.3

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const express = require("express")
 let app = express();
 let port = 8081;
 
+app.set("views", "views")
+app.set("view engine", "ejs")
+
+app.use(express.json())
+
 
 const routerTasks = require("./routers/routerTasks")
 app.use("/tasks", routerTasks)
